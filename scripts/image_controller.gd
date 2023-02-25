8b348a6d6ee1c62d64d717234cf54cc557c7dd66
loregret <dzhukalaevs@gmail.com>
2023-02-25 04:30:33 +0300

    mouse zoom limits added

--- scripts/image_controller.gd
+++ scripts/image_controller.gd
@@ -10,4 +10,9 @@ func _gui_input(event):
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
 			image.scale += Vector2(0.1,0.1)
 			image.update_pivot()
-
+		if event.button_index == MOUSE_BUTTON_MIDDLE and event.pressed:
+			image.scale = Vector2(1, 1)
+			image.update_pivot()
+		image.scale.x = clamp(image.scale.x, 0.35, 1.5)
+		image.scale.y = clamp(image.scale.y, 0.35, 1.5)
+		print(image.scale)
